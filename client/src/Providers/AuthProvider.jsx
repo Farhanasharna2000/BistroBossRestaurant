2ded3339b34b178e3481c0d3b0de7c21de35c81b
Farhanasharna2000 <farhanasharna22@gmail.com>
2025-01-10 22:15:22 +0600

    complete admin home of dashboard

--- client/src/Providers/AuthProvider.jsx
+++ client/src/Providers/AuthProvider.jsx
@@ -3,67 +3,69 @@ import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStat
 import { app } from "../firebase/firebase.config";
 import useAxiosPublic from './../Hooks/useAxiosPublic';
 
-export const AuthContext=createContext(null)
+export const AuthContext = createContext(null)
 
 const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
-    const [user,setUser]=useState(null)
-    const [loading,setLoading]=useState(true)
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
     const googleProvider = new GoogleAuthProvider();
-    const axiosPublic=useAxiosPublic()
-const createUser=(email,password)=>{
-    setLoading(true)
-   return createUserWithEmailAndPassword(auth, email, password)
-}
-const logIn=(email,password)=>{
-    setLoading(true)
-   return signInWithEmailAndPassword(auth, email, password)
-}
-const googleLogIn=()=>{
-    setLoading(true)
-    return  signInWithPopup(auth, googleProvider)
-}
-const logOut=()=>{
-    setLoading(true)
-   return signOut(auth)
-}
-const updateUserProfile=(name,photo)=>{
-   return updateProfile(auth.currentUser, {
-        displayName: name, 
-        photoURL: photo
-      })
-}
-  useEffect(()=>{
-   const unsubscribe= onAuthStateChanged(auth, currentUser => {
-         
+    const axiosPublic = useAxiosPublic()
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const logIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+    const googleLogIn = () => {
+        setLoading(true)
+        return signInWithPopup(auth, googleProvider)
+    }
+    const logOut = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+    const updateUserProfile = (name, photo) => {
+        return updateProfile(auth.currentUser, {
+            displayName: name,
+            photoURL: photo
+        })
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+
             setUser(currentUser)
             // console.log('currentUser',currentUser);
-            if(currentUser){
-//get token and store client
-const userInfo={
-    email:currentUser.email
-}
-axiosPublic.post('jwt',userInfo)
-.then(res=>{
-    if(res.data.token){
-        localStorage.setItem("access-token",res.data.token)
-    }
-})
+            if (currentUser) {
+                //get token and store client
+                const userInfo = {
+                    email: currentUser.email
+                }
+                axiosPublic.post('jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem("access-token", res.data.token)
+                            setLoading(false)
+                        }
+                    })
             }
-            else{
-//remove token(if token stored inthe client side :local storage,caching,in memory)
-localStorage.removeItem("access-token")
+            else {
+                //remove token(if token stored inthe client side :local storage,caching,in memory)
+                localStorage.removeItem("access-token")
+                setLoading(false)
             }
-            
-            setLoading(false)
-       
-      });
-      return ()=>{
-        return unsubscribe()
-      }
-  },[axiosPublic])
-    const authInfo={
+
+           
+
+        });
+        return () => {
+            return unsubscribe()
+        }
+    }, [axiosPublic])
+    const authInfo = {
         user,
         loading,
         createUser,
